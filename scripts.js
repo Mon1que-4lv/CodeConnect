72fe3a41cc558314c24228c4ed6050af7dbb811d
Monique Alves <moniquealv27@gmail.com>
2025-05-27 14:28:49 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,25 @@
+const uploadBtn = document.getElementById('upload-btn');
+const inputUpload = document.getElementById('imagem-upload');
+
+uploadBtn.addEventListener('click', () => {
+    inputUpload.click();
+})
+
+
+
+// document.getElementById('imageUpload').addEventListener('change', function(event) {
+//     var file = event.target.files[0];
+//     // Agora temos o arquivo e podemos fazer mais validações
+// });
+
+// if (!file.type.match('image/png') && !file.type.match('image/jpeg')) {
+//     alert('Por favor, selecione uma imagem PNG ou JPEG.');
+//     return;
+// }
+
+// // Vamos limitar o tamanho a 2MB
+// if (file.size > 2 * 1024 * 1024) {
+//     alert('A imagem deve ter no máximo 2MB.');
+//     return;
+// }
+
